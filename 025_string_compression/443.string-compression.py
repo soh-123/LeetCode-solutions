1f593038101c4a1f3b74807495664a8e94eb18ed
sohier <sohier.lotfy.els@gmail.com>
2023-06-08 23:19:29 +0400

    string compression

---
+++ 025_string_compression/443.string-compression.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=443 lang=python3
+#
+# [443] String Compression
+#
+
+# @lc code=start
+class Solution:
+    def compress(self, chars) -> int:
+        write_index = 0  # Index to write the compressed characters
+        count = 1  # Initialize count of consecutive characters
+        n = len(chars)
+
+        # Iterate from the second character to the end
+        for i in range(1, n + 1):
+            if i < n and chars[i] == chars[i - 1]:
+                count += 1
+            else:
+                chars[write_index] = chars[i - 1]  # Write the character
+
+                # Write the count if it is greater than 1
+                if count > 1:
+                    count_str = str(count)
+                    for j in range(len(count_str)):
+                        chars[write_index + j + 1] = count_str[j]
+                    write_index += len(count_str)
+
+                write_index += 1
+                count = 1
+
+        return write_index
+
+        
+        
+        
+# @lc code=end
+
+nums = ["a","a","b","b","c","c","c"]
+sol = Solution()
+single_num = sol.compress(nums)
+print(single_num)
