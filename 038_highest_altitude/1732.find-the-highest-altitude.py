15b549ae133dd5816a6cef9625dd9e05f081e75f
sohier <sohier.lotfy.els@gmail.com>
2023-06-19 13:48:40 +0400

    1732

---
+++ 038_highest_altitude/1732.find-the-highest-altitude.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1732 lang=python3
+#
+# [1732] Find the Highest Altitude
+#
+
+# @lc code=start
+class Solution:
+    def largestAltitude(self, gain) -> int:
+        current = 0
+        highest = 0
+        for i in gain:
+            current += i
+            highest = max(current, highest)
+        return highest
+# @lc code=end
+
