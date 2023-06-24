1ed9a1bd52d175713d76138a188f1e0abc215ab9
sohier <sohier.lotfy.els@gmail.com>
2023-06-24 15:19:45 +0400

    771

---
+++ 046_jewels_and_stones/771.jewels-and-stones.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=771 lang=python3
+#
+# [771] Jewels and Stones
+#
+
+# @lc code=start
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return len([i for i in stones if i in jewels])
+# @lc code=end
+
