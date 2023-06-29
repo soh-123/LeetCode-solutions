2d71ea0223d8a1c0208b2c763b683286ddce87c0
sohier <sohier.lotfy.els@gmail.com>
2023-06-29 08:03:16 +0400

    804

---
+++ 052_unique_morse_code_words/804.unique-morse-code-words.py
@@ -0,0 +1,60 @@
+#
+# @lc app=leetcode id=804 lang=python3
+#
+# [804] Unique Morse Code Words
+#
+
+# @lc code=start
+class Solution:
+    def uniqueMorseRepresentations(self, words) -> int:
+        morse_dict = {
+    'a': '·−',
+    'b': '−···',
+    'c': '−·−·',
+    'd': '−··',
+    'e': '·',
+    'f': '··−·',
+    'g': '−−·',
+    'h': '····',
+    'i': '··',
+    'j': '·−−−',
+    'k': '−·−',
+    'l': '·−··',
+    'm': '−−',
+    'n': '−·',
+    'o': '−−−',
+    'p': '·−−·',
+    'q': '−−·−',
+    'r': '·−·',
+    's': '···',
+    't': '−',
+    'u': '··−',
+    'v': '···−',
+    'w': '·−−',
+    'x': '−··−',
+    'y': '−·−−',
+    'z': '−−··',
+    '0': '−−−−−',
+    '1': '·−−−−',
+    '2': '··−−−',
+    '3': '···−−',
+    '4': '····−',
+    '5': '·····',
+    '6': '−····',
+    '7': '−−···',
+    '8': '−−−··',
+    '9': '−−−−·',
+    ' ': '/'
+}
+        ans = []
+        for word in range(len(words)):
+            morse_code = ''
+            for letter in words[word]:
+                    morse_code += morse_dict[letter]
+            ans.append(morse_code)    
+        return len(set(ans))
+        # return len(set(' '.join(map(morse_dict.get, words))))
+
+        
+# @lc code=end
+
