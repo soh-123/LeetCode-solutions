a036ef6b3dcc14ed1c02c8289068813711e71e11
sohier <sohier.lotfy.els@gmail.com>
2023-07-01 07:35:45 +0400

    1967

---
+++ 055_find_the_pattern/1967.number-of-strings-that-appear-as-substrings-in-word.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1967 lang=python3
+#
+# [1967] Number of Strings That Appear as Substrings in Word
+#
+
+# @lc code=start
+class Solution:
+    def numOfStrings(self, patterns, word: str) -> int:
+        return len([p for p in patterns if p in word])
+# @lc code=end
+
