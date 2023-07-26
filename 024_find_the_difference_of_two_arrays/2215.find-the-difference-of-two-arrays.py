46cb91c31d6a018964e526456d410c053a77da1f
sohier <sohier.lotfy.els@gmail.com>
2023-07-26 20:32:03 +0400

    2215

---
+++ 024_find_the_difference_of_two_arrays/2215.find-the-difference-of-two-arrays.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=2215 lang=python3
+#
+# [2215] Find the Difference of Two Arrays
+#
+
+# @lc code=start
+class Solution:
+    def findDifference(self, nums1, nums2):
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+
+        sol = [[], []]
+
+        for i in nums1:
+            if i not in nums2:
+                sol[0].append(i)
+
+        for i in nums2:
+            if i not in nums1:
+                sol[1].append(i)
+
+        return sol
+# @lc code=end
+
