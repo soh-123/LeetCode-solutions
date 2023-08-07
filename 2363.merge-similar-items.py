c9650b59298687a2c4674cdee798f3203ee9292e
sohier <sohier.lotfy.els@gmail.com>
2023-08-07 14:21:12 +0400

    apply changes

---
+++ 2363.merge-similar-items.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=2363 lang=python3
+#
+# [2363] Merge Similar Items
+#
+
+# @lc code=start
+class Solution:
+    def mergeSimilarItems(self, items1, items2):
+        ans = []
+        for i in items1:
+            if i[0] in 
+# @lc code=end
+
